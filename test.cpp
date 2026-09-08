24cc4b0508eb7e1925a9340d30922cc215b049a6
saurabhkr78 <meetsaurabh78@gmail.com>
2026-09-08 23:21:36 +0530

    print name 5 times using recursion

--- test.cpp
+++ test.cpp
@@ -1,23 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+void printName(int n){
+    if(n==0) return;
+    cout<<"saurabh\n";
+    printName(n-1);
+}
 int main(){
-    int n;
-    cin>>n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++) cin>>a[i];
-    int large = INT_MIN, second = INT_MIN;
-    for(int i=0;i<n;i++){
-        if(a[i]>large){
-            second = large;
-            large = a[i];
-        }else if(a[i]>second && a[i]!=large){
-            second = a[i];
-        }
-    }
-    if(second==INT_MIN){
-        cout<<"NO SECOND LARGEST\n";
-    }else{
-        cout<<second<<"\n";
-    }
+    printName(5);
     return 0;
 }
